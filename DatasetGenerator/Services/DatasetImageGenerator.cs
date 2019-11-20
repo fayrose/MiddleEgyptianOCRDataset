7355bf319239c2953bdb5566c837a261020c315f
thomas horga <thomas.horga@gmail.com>
2019-11-19 21:05:26 -0500

    ImageCopier Progress

--- DatasetGenerator/Services/DatasetImageGenerator.cs
+++ DatasetGenerator/Services/DatasetImageGenerator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using BitMiracle.Docotic.Pdf;
 
 namespace DatasetGenerator.Services
 {
@@ -13,11 +16,37 @@ namespace DatasetGenerator.Services
         {
             Data = data;
             SplitVygusLocation = pathToSplitVygusFolder;
+            
             OutputLocation = pathToOutputFolder;
         }
 
-        public void GetImagesFromPage(PageData page)
+        public void GetImagesFromFolder()
         {
+            foreach (var file in Directory.EnumerateFiles(SplitVygusLocation))
+            {
+                // TODO : Get the pagenum from file name
+                // Get data.Pages[pagenum - 1]
+                int pageNum = DatasetLabelGenerator.GetPageNumberFromFileName(file);
+
+                var entry = Data.Pages[pageNum - 1];
+                var pdf = new PdfDocument(file);
+                for (int i = 0; i < pdf.Pages.Count; i++)
+                {
+                    string outFileName = pageNum.ToString() + i.ToString();
+
+                    GetImagesFromPage(pdf.Pages[i],entry.EntryData[i], outFileName);
+                }
+            }
+        }
+
+        public void GetImagesFromPage(PdfPage page, EntryData entryData, string outFileName)
+        {
+            ImageCopier imageCopier = new ImageCopier(page);
+            double boundary = entryData.WordBounds.Last() + 5;
+
+            imageCopier.GenerateLineImage(OutputLocation, boundary, outFileName);
+
+
             // Open Page File
             // Foreach entry in page
             // Get entryData's rightmost boundary + some buffer
